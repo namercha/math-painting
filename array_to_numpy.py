417a429f53b039b4996444bf752a38b979a27eeb
nabil <merchant.nabil@gmail.com>
2022-02-01 22:17:27 -0600

    Generating sample images using Pillow.

--- array_to_numpy.py
+++ array_to_numpy.py
@@ -6,5 +6,12 @@ data = np.zeros((5, 4, 3), dtype=np.uint8)
 data[:] = [255, 255, 0]
 print(data)
 
+# Adds a red patch that is horizontal
+data[1:3] = [255, 0, 0]
+
+# Adding more colors
+data[0:3, 0:2] = [255, 200, 233]
+data[3:4, 1:4] = [45, 3, 233]
+
 img = Image.fromarray(data, 'RGB')
 img.save('canvas.png')
